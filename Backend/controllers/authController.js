d7c9fb17baabfb6d2830f0dd55f7bfef6fec66d2
Sandeep Biswal <samdeepbiswal123@gmail.com>
2023-11-16 22:55:56 +0530

    Sign in Functionality Added

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -3,6 +3,7 @@ import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
 import jwt from "jsonwebtoken";
 
+//signup logic controller
 export const signup = async (req,res,next) =>{
   const {username,email, password} = req.body;
   const hashedPassword = bcryptjs.hashSync(password,10);
@@ -17,9 +18,10 @@ export const signup = async (req,res,next) =>{
   }
 }
 
-
+// sigin logic controller
 export const signin =async (req, res, next) =>{
   const {email, password} = req.body;
+
   try {
   const validUser = await User.findOne({email});
   if(!validUser) return next(errorHandler(404, "User not Found !!"));
@@ -27,10 +29,12 @@ export const signin =async (req, res, next) =>{
   if(!validPassword) return next(errorHandler(401, "Wrong Credentials !!"));
   const token  = jwt.sign({_id: validUser._id}, process.env.JWT_SECRET);
   const {password:pass, ...rest} = validUser._doc;
+
   res.cookie('access_token', token, {httpOnly: true,})
   .status(200).json(rest);
 
-  } catch (error) {
+  } 
+  catch (error) {
     next(error);
   }
 }
--- Frontend/src/pages/Signin.jsx
+++ Frontend/src/pages/Signin.jsx
@@ -1,21 +1,49 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
+// import { useDispatch, useSelector } from 'react-redux';
 import OAuth from "../components/oAuth";
 
 const signin = () => {
-  const [formData, setFormData] = useState({});
-  const [loading, error] = useState()
+  const [formData, setformData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
+  const handleChange = (e) =>{
+    setformData({
+      ...formData, 
+      [e.target.id] : e.target.value,
+    });
+  };
 
-  const handleChange =(e) =>{
-    setFormData ({
-      ...formData,
-      [e.target.id] : e.target.value
-    })
-  }
-  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch('/api/auth/signIn', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+      console.log(data);
+
+      if (data.success === false) {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      // setError(null);
+      navigate('/');
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
   return (
     <div><>
     <div className="py-16">
@@ -40,13 +68,16 @@ const signin = () => {
             </a>
             <span className="border-b w-1/5 lg:w-1/4" />
           </div>
+          <form onSubmit={handleSubmit}>
           <div className="mt-4">
             <label className="block text-gray-700 text-sm font-bold mb-2">
               Email Address
             </label>
+            
             <input
               className="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none"
               type="email"
+              id='email'
               onChange={handleChange}
             />
           </div>
@@ -62,19 +93,24 @@ const signin = () => {
             <input
               className="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none"
               type="password"
+              onChange={handleChange}
+              id='password'
             />
           </div>
           <div className="mt-8">
-            <button className="bg-gray-700 text-white font-bold py-2 px-4 w-full rounded hover:bg-gray-600">
-              Sign in
+            <button disabled={loading} className="bg-gray-700 text-white font-bold py-2 px-4 w-full rounded hover:bg-gray-600">
+         {loading ? 'Loading...' : 'sign in'}
+              
             </button>
           </div>
+            </form>
           <div className="mt-4 flex items-center justify-between">
             <span className="border-b w-1/5 md:w-1/4" />
             <a href="/signUp" className="text-xs text-gray-500 uppercase">
               or sign up
             </a>
             <span className="border-b w-1/5 md:w-1/4" />
+            {error && <p className='text-red-500 mt-5'>{error}</p>}
           </div>
         </div>
       </div>
